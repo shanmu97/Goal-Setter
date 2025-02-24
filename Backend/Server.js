462baa5483d4bba33101076541b648a47958abc7
Shanmukha Reddy Vasa <shanmukhareddyvasa@gmail.com>
2025-02-24 10:44:35 +0530

    Cors added

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const {connectDB}  = require('./config/db')
 const {errorHandler} = require('./MIddleWare/errorMiddleware')
+const cors = require('cors')
 
 connectDB()
 
@@ -9,9 +10,9 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors({origin:'*'}))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
 app.use('/api/goals',require("./Routes/getRoutes"))
 app.use('/api/users',require("./Routes/userRoutes"))
 
