bceccc50fb8fa502596cb7d2afa35ecc0e342001
Shanmukha Reddy Vasa <shanmukhareddyvasa@gmail.com>
2025-02-28 16:40:21 +0530

    Route error removed

--- Backend/Routes/getRoutes.js
+++ Backend/Routes/getRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {getGoals,setGoals,putGoals,deleteGoals} = require("../Controller/goalController")
 const router = express.Router()
 const {protect} = require('../MIddleWare/authMiddleware')
-router.route('/',).get(protect, getGoals).post(protect,setGoals)
+router.route('/').get(protect, getGoals).post(protect,setGoals)
 
 router.route('/:id').put(protect,putGoals).delete(protect,deleteGoals)
 
